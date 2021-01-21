ab2999587d359e36b7508506cea18f08cdc64622
Simeon Willard <swillard4597@gmail.com>
2021-01-21 16:09:23 -0600

    updated buttons

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,8 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function PizzaList() {
+
+    const [clickedAdd, setClickedAdd] = useState(false);
+    const [buttonText, setButtonText] = useState('Add');
+    const [addPrice, setAddPrice] = useState(0);
 
     const pizzaList = useSelector(state => state.pizzaList);
     console.log(pizzaList);
@@ -27,19 +32,32 @@ function PizzaList() {
         console.log('in useEffect');
         getPizza();
     }, []);
-    
+
+    const handleAdd = (event) => {
+        console.log('clicked add');
+        setClickedAdd(!clickedAdd);
+        setButtonText('Remove');
+
+        // filtering through pizzaList to find the id of the pizza we clicked on
+        // then sending that pizza.price to the reducer
+        const payload = pizzaList.filter((pizza) => pizza.id === event)
+        dispatch({type: 'TOTAL_PRICE', payload: Number(payload[0].price)});
+
+    }
 
     return (
         <div>
-            {pizzaList.map((pizza, i) => (
-                <div key={i}>
+            {pizzaList.map((pizza) => (
+        
+                <div key={pizza.id}>
                     <div><img src={pizza.image_path} />
                     <div><h4><b>{pizza.name}</b></h4>
                     <p>{pizza.description}</p>
                     <p>Price: {pizza.price}</p>
                     </div>
                     </div>
-                </div>
+                    <button onClick={(event) => handleAdd(pizza.id)}>{buttonText}</button>
+                </div>  
             ))}
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -35,8 +35,13 @@ const pizzaList = (state = [], action) => {
 
 
 const priceTotalReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'TOTAL_PRICE':
+            return state + action.payload;
 
-    return state;
+        default:
+            return state;
+    }
 }
 
 
