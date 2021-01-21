0cbaa21898a0d00584e82bafe38718661acb00a3
Simeon Willard <swillard4597@gmail.com>
2021-01-21 14:04:27 -0600

    made reducers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customerReducer = (state = {}, action) {
+    return state;
+}
+
+
+
+
+const pizzaList = (state = [], action) => {
+    return state;
+}
+
+
+
+
+
+
+const priceTotalReducer = (state = 0, action) => {
+
+    return state;
+}
+
+
+
+
+
+
+const ordersReducer = (state = [], action) {
+    return state;
+}
+
+
+
+
+
+
+const storeInstance = createStore(
+    combineReducers({
+        ordersReducer,
+        priceTotalReducer,
+        pizzaList,
+        customerReducer
+    }),
+    applyMiddleware(logger)
+);
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
