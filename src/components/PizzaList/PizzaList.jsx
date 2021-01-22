79df104cd4c49bac4f32740f9b086cff16b16287
Justin Appelgren <justinappelgren@gmail.com>
2021-01-22 10:40:56 -0600

    Style

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -26,60 +26,58 @@ function PizzaList() {
       });
   }
 
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const history = useHistory();
 
   // const handleAdd = (event) => {
 
-    const handleNext = () => {
-        history.push('/customerForm');
-    }
+  const handleNext = () => {
+    history.push('/customerForm');
+  };
 
-    useEffect(() => {
-        console.log('in useEffect');
-        getPizza();
-    }, []);
+  useEffect(() => {
+    console.log('in useEffect');
+    getPizza();
+  }, []);
 
+  // const handleAdd = (event) => {
 
-    // const handleAdd = (event) => {
+  // filtering through pizzaList to find the id of the pizza we clicked on
+  // then sending that pizza.price to the reducer
+  // const payload = pizzaList.filter((pizza) => pizza.id === event)
+  // dispatch({ type: 'TOTAL_PRICE', payload: Number(payload[0].price) });
+  // const pizzaload = pizzaList.filter((pizza) => pizza.id === event)
+  // dispatch({ type: 'ADD_CART', payload: pizzaload });
 
-    // filtering through pizzaList to find the id of the pizza we clicked on
-    // then sending that pizza.price to the reducer
-    // const payload = pizzaList.filter((pizza) => pizza.id === event)
-    // dispatch({ type: 'TOTAL_PRICE', payload: Number(payload[0].price) });
-    // const pizzaload = pizzaList.filter((pizza) => pizza.id === event)
-    // dispatch({ type: 'ADD_CART', payload: pizzaload });
+  //     // filtering through pizzaList to find the id of the pizza we clicked on
+  //     // then sending that pizza.price to the reducer
+  //     const payload = pizzaList.filter((pizza) => pizza.id === event);
+  //     dispatch({type: 'TOTAL_PRICE', payload: Number(payload[0].price)});
 
-    //     // filtering through pizzaList to find the id of the pizza we clicked on
-    //     // then sending that pizza.price to the reducer
-    //     const payload = pizzaList.filter((pizza) => pizza.id === event);
-    //     dispatch({type: 'TOTAL_PRICE', payload: Number(payload[0].price)});
+  // }
 
-
-    // }
-
-    return (
-        <div>
-            {pizzaList.map((pizza) => (
-
-                <div key={pizza.id}>
-                    <div><img src={pizza.image_path} />
-                        <div><h4><b>{pizza.name}</b></h4>
-                            <p>{pizza.description}</p>
-                            <p>Price: {pizza.price}</p>
-                        </div>
-                    </div>
-                    {/* {clickedAdd ? <button>Remove</button> : <button onClick={(event) => handleAdd(pizza.id)}>Add</button>} */}
-                    <PizzaItem pizza={pizza} />
-
-                </div>
-            ))}
-        <button type="submit" onClick={handleNext}>
-            Next
-        </button>
+  return (
+    <div className="menu-container">
+      {pizzaList.map((pizza) => (
+        <div className="pizza-card" key={pizza.id}>
+          <div>
+            <img className="pizza-img" src={pizza.image_path} />
+            <div className="pizza-info">
+              <h4>
+                <b>{pizza.name}</b>
+              </h4>
+              <p>{pizza.description}</p>
+              <p>Price: {pizza.price}</p>
+            </div>
+          </div>
+          {/* {clickedAdd ? <button>Remove</button> : <button onClick={(event) => handleAdd(pizza.id)}>Add</button>} */}
+          <PizzaItem pizza={pizza} />
         </div>
-    )
-
+      ))}
+      <button type="submit" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
 }
 
 export default PizzaList;
